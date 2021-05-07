e0c50eec0d72fce8e4a7bca4298ba37ca909596c
carsault <tristan.carsault@gmail.com>
2021-05-07 15:37:10 +0200

    initial commit

---
+++ createDataset.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 15:31:58 2019
+
+@author: carsault
+"""
+#%%
+import argparse
+import os
+import numpy as np
+import torch
+import torch.utils.data as data_utils
+from torch.utils.data.dataset import *
+from torch.utils import data
+import pickle
+from sklearn.model_selection import train_test_split
+import random
+from utilities import chordUtil
+from utilities.chordVocab import *
+from utilities import utils
+from utilities import ACEdataImport
+
+parser = argparse.ArgumentParser(description='ACE dataset')
+parser.add_argument('--dataFolder',   type=str,   default='okcomputer',    help='name of the data folder')
+parser.add_argument('--alpha',      type=str,   default='a0', help='type of alphabet')
+parser.add_argument('--random_state',   type=int,   default=123456,    help='seed for the random train/test split')
+args = parser.parse_args()
+print(args)
+
+args.dataFolder = args.alpha + "_ACE_" + str(args.random_state)
+
+dictChord, listChord = chordUtil.getDictChord(eval(args.alpha))
+
+# List files
+filenames = os.listdir("Datas/specs_train")
+filenames.remove(".DS_Store")
+filenames = [ x for x in filenames if "_lab.npy" not in x ]
+filenames = [ x.replace('.npy','')for x in filenames]
+filenames.sort()
+
+sizeOfPartTrain = []
+sizeOfPartValid = []
+sizeOfPartTest = []
+
+
+# Create datasets
+files_train ,files_test = train_test_split(filenames,test_size=0.2,random_state=args.random_state)
+files_test ,files_valid = train_test_split(files_test,test_size=0.5,random_state=args.random_state)
+
+dictFilenameTrain = {}
+for i in files_train:
+    listOfFrame  = []
+    cqt = np.load("Datas/specs_train/"+ i + ".npy")
+    listOfFrame = list(range(len(cqt)-15))
+    random.shuffle(listOfFrame)
+    dictFilenameTrain[i] = listOfFrame
+    for j in range(len(listOfFrame)):
+        sizeOfPartTrain.append(i)
+    #print(list(range(len(cqt)-15)))
+#%%    
+dictFilenameValid = {}
+for i in files_valid:
+    listOfFrame  = []
+    cqt = np.load("Datas/specs_train/"+ i + ".npy")
+    listOfFrame = list(range(len(cqt)-15))
+    random.shuffle(listOfFrame)
+    dictFilenameValid[i] = listOfFrame
+    for j in range(len(listOfFrame)):
+        sizeOfPartValid.append(i)
+    #print(list(range(len(cqt)-15)))    
+    
+dictFilenameTest = {}
+for i in files_test:
+    listOfFrame  = []
+    cqt = np.load("Datas/specs_train/"+ i + ".npy")
+    listOfFrame = list(range(len(cqt)-15))
+    random.shuffle(listOfFrame)
+    dictFilenameTest[i] = listOfFrame
+    for j in range(len(listOfFrame)):
+        sizeOfPartTest.append(i)
+    #print(list(range(len(cqt)-15)))
+#%%
+ 
+
+random.shuffle(sizeOfPartTrain)
+random.shuffle(sizeOfPartValid)
+random.shuffle(sizeOfPartTest)
+randTransp = ACEdataImport.randomTranspose(dictChord, listChord, args) 
+#randTransp = None
+ACEdataImport.datasetSaved(files_train, "train", sizeOfPartTrain, dictFilenameTrain, args, dictChord, listChord, randTransp)
+ACEdataImport.datasetSaved(files_valid, "valid", sizeOfPartValid, dictFilenameValid, args, dictChord, listChord)
+ACEdataImport.datasetSaved(files_test, "test", sizeOfPartTest, dictFilenameTest, args, dictChord, listChord)
---
+++ transfData.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 15:31:58 2019
+
+@author: carsault
+"""
+#%%
+import argparse
+import numpy as np
+from librosa.core import cqt,load,note_to_hz
+from librosa.util import find_files
+from utilities.utils import LoadLabelArr
+from utilities import chordUtil
+from utilities.chordVocab import *
+#import const as C
+import os
+
+parser = argparse.ArgumentParser(description='ACE dataset')
+parser.add_argument('--alpha',      type=str,   default='a0', help='type of alphabet')
+args = parser.parse_args()
+print(args)
+dictChord, listChord = chordUtil.getDictChord(eval(args.alpha))
+#%%
+
+audiolist = find_files("Datas/audio_train")
+sr = 44100
+path_hcqt = "Datas/specs_train"
+itemcnt = len(audiolist)
+i = 0
+#%%
+for audiofile in audiolist:
+    labelfile = audiofile.replace('.wav','.lab')
+    labelfile = labelfile.replace('audio_train','labels_train')
+    labelfile = labelfile.replace(' ','_')
+    labelfile = labelfile.replace(',','')
+    #labelfile = labelfile.replace('.','._')
+    i += 1
+    print("Processing %d/%d" % (i,itemcnt))    
+    wav,sr = load(audiofile,sr=sr)
+    print(sr)
+    #fmin = note_to_hz("C2")
+    fmin = 65
+    #spec = np.stack([np.abs(cqt(wav,sr=C.SR,hop_length=512,n_bins=C.BIN_CNT,bins_per_octave=C.OCT_BIN,fmin=fmin*(h+1),filter_scale=2,tuning=None)).T.astype(np.float32) for h in range(C.CQT_H)])
+    spec = np.abs(cqt(wav,sr=sr,hop_length=512*4,n_bins=105,bins_per_octave=24,fmin=fmin,filter_scale=2,tuning=None)).T.astype(np.float32)
+    print(len(spec))
+    print(len(spec[0]))
+    lab = LoadLabelArr(labelfile,dictChord,args,512*4)
+    filename = audiofile.split('/')[-1].split(".")[0]
+    savepath = os.path.join(path_hcqt,filename+".npy")
+    np.save(savepath,spec)
+    savepath = os.path.join(path_hcqt,filename+"_lab.npy")
+    np.save(savepath,lab)
+
+#%%
